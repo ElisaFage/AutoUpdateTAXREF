bf556a351c29b52214ae626892fe2c793215111f
elisa fage <EFA@sage.local>
2025-04-01 23:10:19 +0200

    ProgressDownload mis en PEP8 et optimisé

--- ProgressDownload.py
+++ ProgressDownload.py
@@ -157,9 +157,9 @@ class DownloadWindow(QWidget):
         if new_version:
             self._start_get_url()
         elif new_status:
-            self._start_download_status()
+            self.__start_download_status()
         else:
-            self._start_save_sources()
+            self.__start_save_sources()
 
     def _filter_taxa(self, regne: str):
         """Filtrer les taxons par règne."""
@@ -198,100 +198,79 @@ class DownloadWindow(QWidget):
         """
         Annule le processus de téléchargement et ferme la fenêtre.
         """
-        self.cancel_flag = True
         self.cancel_requested.emit()  # Émet un signal pour annuler les threads en cours
-        self.update_progress_text("Annulation en cours...")
+        self.update_global_progress_label("Annulation en cours...")
         self.close()
 
-    def update_progress_text(self, text):
+    def update_global_progress_label(self, text):
         """
         Met à jour le texte du label principal.
 
         :param text: Texte à afficher.
         """
-        if hasattr(self, 'progressLabel'):
-            self.progressLabel.setText(text)
+        if hasattr(self, 'global_progress_label'):
+            self.global_progress_label.setText(text)
 
-    def update_secondary_progress_text(self, text):
+    def update_step_progress_label(self, text):
         """
         Met à jour le texte du label secondaire.
 
         :param text: Texte à afficher.
         """
-        if hasattr(self, 'progressLabelSecondary'):
-            self.progressLabelSecondary.setText(text)
+        if hasattr(self, 'current_step_progress_label'):
+            self.current_step_progress_label.setText(text)
 
-    def start_getURL(self):
+    def _start_get_url(self):
         """
         Démarre le thread permettant de récupérer l'URL de téléchargement.
         """
-        self.geturl_thread = GetURLThread(self.version)
-        self.geturl_thread.finished.connect(self.url_found)
-        self.cancel_requested.connect(self.geturl_thread.terminate)  # Connecte le signal d'annulation
-        self.update_secondary_progress_text(
+        self.get_url_thread = GetURLThread(self.version)
+        self.get_url_thread.finished.connect(self._on_url_found)
+        self.cancel_requested.connect(self.get_url_thread.terminate)  # Connecte le signal d'annulation
+        self.update_step_progress_label(
             f"Progression globale (étapes {int(self.current_step + 1)}/{self.n_step})"
         )
-        self.geturl_thread.start()
+        self.get_url_thread.start()
 
-    def url_found(self, url):
+    def _on_url_found(self, url):
         """
         Callback appelé lorsque l'URL de téléchargement est récupérée.
 
         :param url: URL récupérée.
         """
-        self.url = url
-        self.current_step += 1
-        secondary_progress_value = int(round(100 * self.current_step / self.n_step))
-        self.progressBarSecondary.setValue(secondary_progress_value)
+        self.file_url = url
+        self._increment_step()
+        self._start_download()
 
-        if secondary_progress_value >= 100:
-            self.progressBarSecondary.setValue(100)
-            self.close()
-            return
-
-        self.start_download()
-
-    def start_download(self):
+    def _start_download(self):
         """
         Démarre le thread de téléchargement du fichier TAXREF.
         """
         self.download_thread = DownloadThread(self.url)
-        self.download_thread.progress.connect(self.progressBar.setValue)
-        self.download_thread.finished.connect(self.download_complete)
+        self.download_thread.progress.connect(self.current_step_progress_bar.setValue)
+        self.download_thread.finished.connect(self._on_download_complete)
         self.cancel_requested.connect(self.download_thread.terminate)
-        self.update_secondary_progress_text(
+        self.update_step_progress_label(
             f"Progression globale (étapes {int(self.current_step + 1)}/{self.n_step})"
         )
-        self.update_progress_text("Téléchargement de TAXREF en cours...")
+        self.update_global_progress_label("Téléchargement de TAXREF en cours...")
         self.download_thread.start()
 
-    def download_complete(self, file_path):
+    def _on_download_complete(self, file_path):
         """
         Callback appelé lorsque le téléchargement est terminé.
 
         :param file_path: Chemin du fichier téléchargé.
         """
         self.file_path = file_path
-        self.progressBar.setValue(100)
-        self.update_progress_text("Téléchargement de TAXREF terminé")
+        self._increment_step()
+        self._start_save_taxref()
 
-        # Mise à jour de la progression secondaire
-        self.current_step += 1
-        secondary_progress_value = int(round(100 * self.current_step / self.n_step))
-        self.progressBarSecondary.setValue(secondary_progress_value)
-
-        if secondary_progress_value >= 100:
-            self.progressBarSecondary.setValue(100)
-            self.close()
-            return
-
-        self.start_save_taxref()
-
-    def start_save_taxref(self):
+    def _start_save_taxref(self):
         """
         Démarre le thread de sauvegarde des données TAXREF.
         """
-        self.majtaxref_thread = MajTaxrefThread(
+        self.save_taxref_thread = MajTaxrefThread(
             self.file_path,
             self.version,
             self.taxon_titles,
@@ -303,29 +282,21 @@ class DownloadWindow(QWidget):
             self.save_path,
             self.synonyme
         )
-        self.majtaxref_thread.finished.connect(self.taxref_saved)
-        self.cancel_requested.connect(self.majtaxref_thread.terminate)
-        self.update_secondary_progress_text(
+        self.save_taxref_thread.finished.connect(self._on_taxref_saved)
+        self.cancel_requested.connect(self.save_taxref_thread.terminate)
+        self.update_step_progress_label(
             f"Progression globale (étapes {int(self.current_step + 1)}/{self.n_step})"
         )
-        self.majtaxref_thread.start()
+        self.save_taxref_thread.start()
 
-    def taxref_saved(self):
+    def _on_taxref_saved(self):
         """
         Callback appelé lorsque l'enregistrement des données TAXREF est terminé.
         """
-        self.current_step += 1
-        secondary_progress_value = int(round(100 * self.current_step / self.n_step))
-        self.progressBarSecondary.setValue(secondary_progress_value)
+        self._increment_step()
+        self._start_download_status()
 
-        if secondary_progress_value >= 100:
-            self.progressBarSecondary.setValue(100)
-            self.close()
-            return
-
-        self.start_download_status()
-
-    def start_download_status(self):
+    def _start_download_status(self):
         """
         Démarre le thread de téléchargement des statuts.
         """
@@ -337,46 +308,43 @@ class DownloadWindow(QWidget):
             self.folder_excel,
             debug=self.debug
         )
-        self.get_status_thread.progress.connect(self.progressBar.setValue)
-        self.get_status_thread.finished.connect(self.start_save_sources)
+        self.get_status_thread.progress.connect(self.current_step_progress_bar.setValue)
+        self.get_status_thread.finished.connect(self._start_save_sources)
         self.cancel_requested.connect(self.get_status_thread.termination_process)
-        self.update_secondary_progress_text(
+        self.update_step_progress_label(
             f"Progression globale (étapes {int(self.current_step + 1)}/{self.n_step})"
         )
-        self.update_progress_text("Téléchargement des statuts en cours...")
+        self.update_global_progress_label("Téléchargement des statuts en cours...")
         self.get_status_thread.start()
 
-    def start_save_sources(self):
+    def _start_save_sources(self):
         """
         Démarre le thread de sauvegarde des sources.
         """
-        self.current_step += 1
-        secondary_progress_value = int(round(100 * self.current_step / self.n_step))
-        self.progressBarSecondary.setValue(secondary_progress_value)
-        self.update_progress_text("Téléchargement des statuts terminé")
+        self._increment_step()
+        self.update_global_progress_label("Téléchargement des statuts terminé")
 
         QgsMessageLog.logMessage("In start save sources", "AutoUpdateTAXREF", level=Qgis.Info)
         self.save_sources_thread = SaveSourcesThread(
             self.save_path, self.new_version, self.new_sources
         )
-        self.save_sources_thread.finished.connect(self.sources_saved)
+        self.save_sources_thread.finished.connect(self._on_sources_saved)
         self.cancel_requested.connect(self.save_sources_thread.terminate)
-        self.update_secondary_progress_text(
-            f"Progression globale (étapes {int(self.current_step + 1)}/{self.n_step})"
+        self.update_step_progress_label(
+            f"Progression globale (étapes {int(self.current_step + 1)}/{self.total_steps})"
         )
         self.save_sources_thread.start()
 
-    def sources_saved(self):
+    def _on_sources_saved(self):
         """
         Callback appelé lorsque l'enregistrement des sources est terminé.
         """
         QgsMessageLog.logMessage("In sources saved", "AutoUpdateTAXREF", level=Qgis.Info)
-        self.current_step += 1
-        secondary_progress_value = int(round(100 * self.current_step / self.n_step))
-        self.progressBarSecondary.setValue(secondary_progress_value)
+        self._increment_step()
+        self.close()
 
-        if secondary_progress_value >= 100:
-            self.progressBarSecondary.setValue(100)
-            self.close()
-        else:
-            self.close()
+    def _increment_step(self):
+        """Met à jour la barre de progression globale."""
+        self.current_step += 1
+        progress = int(100 * self.current_step / self.total_steps)
+        self.global_progress_bar.setValue(progress)
