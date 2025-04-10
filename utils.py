b136331057652c2f92e3f6a6c9d461cc0945a302
ChristopherAllene <christopher.allene@wanadoo.fr>
2025-04-10 18:33:28 +0200

    Ajoute de get_file_save_path à utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from qgis.core import QgsMessageLog, Qgis
 from datetime import datetime
+import os
 
 def print_debug_info(debug_level, debug_threshold, msg):
 
@@ -8,3 +9,12 @@ def print_debug_info(debug_level, debug_threshold, msg):
         QgsMessageLog.logMessage(msg+f" ({now.hour:02}:{now.minute:02}:{now.second:02}.{now.microsecond // 1000:03})", "AutoUpdateTAXREF", level=Qgis.Info)
 
     return
+
+def get_file_save_path(path, taxon_title):
+    
+    if taxon_title == "Flore":
+        file_save_path = os.path.join(path, f"{taxon_title}.gpkg")
+    else : 
+        file_save_path = os.path.join(path, f"Faune.gpkg")
+
+    return file_save_path
